c2099235472aa0962d8cdec24efbc5b25365826e
not-pen4ik <pen.tv1@yandex.kz>
2026-02-16 22:40:13 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true, // Отключаем фейсконтроль для ngrok
+    allowedHosts: true,
   }
 })
